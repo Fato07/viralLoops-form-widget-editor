f8b5ed522cedce7f209e82cbd25336f495ea2f6c
Sha256_Nulled <Fathindos.FD@gmail.com>
2023-10-20 15:10:18 +0300

    feat: initial version of sidebar component

---
+++ src/components/Layout.tsx
@@ -0,0 +1,18 @@
+import { Box, Flex } from '@chakra-ui/react';
+import Sidebar from './Sidebar';
+
+interface LayoutProps {
+ children: React.ReactNode;
+}
+const Layout: React.FC<LayoutProps> = ({ children }) => {
+ return (
+  <Flex>
+   <Sidebar />
+   <Box flex="1" p="4">
+    {children}
+   </Box>
+  </Flex>
+ );
+};
+
+export default Layout;
--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,9 +1,63 @@
-import React from 'react'
+import { Box, Heading, VStack, FormControl, FormLabel, Input, ColorModeScript, Button, Select } from '@chakra-ui/react';
+import styled from 'styled-components';
+
+const StyledSidebar = styled(Box)`
+    width: 300px;
+    padding: 20px;
+    border-right: 1px solid #e2e8f0;
+`;
 
 const Sidebar = () => {
  return (
-  <div>Sidebar</div>
- )
-}
+  <StyledSidebar>
+   <VStack spacing={4} align="stretch">
+    {/* Title and Subtitle */}
+    <Heading size="md">Form Settings</Heading>
+    <FormControl>
+     <FormLabel>Title</FormLabel>
+     <Input placeholder="Enter title" />
+    </FormControl>
+    <FormControl>
+     <FormLabel>Subtitle</FormLabel>
+     <Input placeholder="Enter subtitle" />
+    </FormControl>
 
-export default Sidebar
+    {/* Font Customization */}
+    <Heading size="md">Font Customization</Heading>
+    <FormControl>
+     <FormLabel>Font Size</FormLabel>
+     <Select placeholder="Select font size">
+      <option value="small">Small</option>
+      <option value="medium">Medium</option>
+      <option value="large">Large</option>
+     </Select>
+    </FormControl>
+    <FormControl>
+     <FormLabel>Font Color</FormLabel>
+     <Input type="color" />
+    </FormControl>
+    <FormControl>
+     <FormLabel>Text Alignment</FormLabel>
+     <Select placeholder="Select alignment">
+      <option value="left">Left</option>
+      <option value="center">Center</option>
+      <option value="right">Right</option>
+     </Select>
+    </FormControl>
+
+    {/* Submit Button Customization */}
+    <Heading size="md">Submit Button</Heading>
+    <FormControl>
+     <FormLabel>Background Color</FormLabel>
+     <Input type="color" />
+    </FormControl>
+    <FormControl>
+     <FormLabel>Button Text</FormLabel>
+     <Input placeholder="Enter button text" />
+    </FormControl>
+   </VStack>
+  </StyledSidebar>
+ );
+};
+
+export default Sidebar;
