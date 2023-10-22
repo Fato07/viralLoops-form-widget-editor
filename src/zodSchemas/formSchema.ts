8cd8faf5613d6c17e6ceaad0fef0e5669e5bf3f7
Sha256_Nulled <Fathindos.FD@gmail.com>
2023-10-22 12:58:33 +0300

    feat: formSchema update (Zod Validation)

--- src/zodSchemas/formSchema.ts
+++ src/zodSchemas/formSchema.ts
@@ -1,8 +1,8 @@
 import { z } from 'zod';
 
 export const formSchema = z.object({
- title: z.string().min(1, { message: 'Title is required' }),
- subtitle: z.string(),
+  title: z.string(),
+  subtitle: z.string(),
   fontSize: z.enum(['small', 'medium', 'large']),
   fontColor: z.string().regex(/^#([0-9A-F]{3}){1,2}$/i, { message: 'Invalid color code' }),
   textAlign: z.enum(['left', 'center', 'right']),
@@ -12,6 +12,13 @@ export const formSchema = z.object({
     z.object({
       id: z.string(),
       type: z.enum(['checkbox', 'radio', 'dropdown']),
-      label: z.string().min(1, { message: 'Label is required' }),
+      label: z.string(),
+      required: z.boolean().optional(),
+      options: z.array(
+        z.object({
+          label: z.string(),
+          value: z.string(),
+        }
+      )).optional(),
     })).optional(),
 });
