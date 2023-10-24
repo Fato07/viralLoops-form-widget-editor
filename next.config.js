e65d3282d3db427312a5671b799faabd736072ef
Sha256_Nulled <Fathindos.FD@gmail.com>
2023-10-25 01:01:30 +0300

    some fix for deployment issue

--- next.config.js
+++ next.config.js
@@ -1,14 +1,6 @@
-const withPWA = require('next-pwa')({
-  dest: 'public'
-})
-
 /** @type {import('next').NextConfig} */
-const nextConfig = withPWA({
+const nextConfig = {
   reactStrictMode: true,
-  pwa: {
-    disable: process.env.NODE_ENV === 'development',
-    swSrc: 'worker.js'
-  }
-})
+}
 
 module.exports = nextConfig
