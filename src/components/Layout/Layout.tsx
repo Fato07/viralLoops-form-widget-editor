4f1cdc84b2e4b9a79540a1078e8a6e9a835ccd7f
Sha256_Nulled <Fathindos.FD@gmail.com>
2023-10-24 23:58:03 +0300

    feat: refactoring + auth logic

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -1,11 +1,14 @@
 import {
-  Box, Flex
+  Box, Flex, Text
 } from '@chakra-ui/react';
-import Sidebar from './sidebar/Sidebar';
-import Preview from './preview/Preview';
-import Footer from './Footer/Footer';
+import Sidebar from '../sidebar/Sidebar';
+import Preview from '../preview/Preview';
+import Footer from '../Footer/Footer';
+import { useUser } from "@clerk/nextjs";
 
 const Layout = () => {
+
+  const { isLoaded, isSignedIn, user } = useUser();
 
   return (
     <Flex minHeight="100vh">
@@ -13,7 +16,8 @@ const Layout = () => {
         <Sidebar />
       </Box>
       <Flex flexDirection="column" flex="1">
-        <Box flex="1" p="4" display="flex" justifyContent="center" alignItems="center">
+
+        <Box flex="1" p="4" display="flex" height="700px" justifyContent="center" alignItems="center">
           <Preview />
         </Box>
         <Footer />
