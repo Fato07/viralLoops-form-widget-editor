5fbff055ed2a888dfd7a8eb6f99d3cf3386d9bba
Sha256_Nulled <Fathindos.FD@gmail.com>
2023-10-25 16:01:49 +0300

    feat: deployimng to check file path

---
+++ src/pages/api/processWidget.ts
@@ -0,0 +1,51 @@
+import { NextApiRequest, NextApiResponse } from 'next';
+import { Redis } from '@upstash/redis';
+import { PrismaClient } from '@prisma/client';
+import { put } from "@vercel/blob";
+import Handlebars from "handlebars";
+import fs from 'fs';
+import path from 'path';
+
+const prisma = new PrismaClient();
+const redis = Redis.fromEnv();
+
+// Register 'switch' helper
+Handlebars.registerHelper('switch', function(this:any, value: any, options: any) {
+ this._switch_value_ = value;
+ const html = options.fn(this); // Process the body of the switch block
+ delete this._switch_value_;
+ return html;
+});
+
+// Register 'case' helper
+Handlebars.registerHelper('case', function(this:any, value: any, options: any) {
+ if (value == this._switch_value_) {
+     return options.fn(this);
+ }
+});
+
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+
+ const task = await redis.lmove('widget-settings-tasks', 'widget-settings-tasks-processing', 'right', 'left');
+
+ if (!task) {
+  return res.status(200).send("No tasks to process.");
+ }
+
+ console.log('FILE PATH', process.cwd());
+
+ const widgetSettingsData = task;
+ // Path to the Handlebars template
+ // const templatePath = path.join(process.cwd(), 'templates', 'widget.handlebars');
+  
+ // // Read the template
+ // const templateString = fs.readFileSync(templatePath, 'utf8');
+
+ // // Compile the template with Handlebars
+ // const template = Handlebars.compile(templateString);
+
+
+
+
+
+}
